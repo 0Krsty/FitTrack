c579eb92011bd517e59cfbb7388acad2529b954e
0Krsty <163340665+0Krsty@users.noreply.github.com>
2024-06-04 02:33:09 +0300

    new feature

--- frontend/ActivityList.tsx
+++ frontend/ActivityList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchAllActivities, removeActivityById } from './activitiesSlice';
 import { RootState } from './store';
@@ -13,6 +13,7 @@ interface FitnessActivity {
 const FitnessActivitiesList: React.FC = () => {
   const dispatch = useDispatch();
   const activitiesList = useSelector((state: RootState) => state.activities.list);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     dispatch(fetchAllActivities());
@@ -20,18 +21,28 @@ const FitnessActivitiesList: React.FC = () => {
 
   const handleDeleteActivity = (activityId: string) => {
     dispatch(removeActivityById(activityId));
-  }
+  };
+
+  const filteredActivities = activitiesList.filter(activity =>
+    activity.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <div>
       <h2>Fitness Activities</h2>
+      <input
+        type="text"
+        placeholder="Filter activities by name..."
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
       <ul>
-        {activitiesList.map((activity: FitnessActivity) => (
+        {filteredActivities.map((activity: FitnessActivity) => (
           <li key={activity.id}>
             <p>Name: {activity.name}</p>
             <p>Duration: {activity.duration}</p>
             <p>Date: {activity.date}</p>
-            <button onClick={() => {}}>Edit</button> 
+            <button onClick={() => {}}>Edit</button>
             <button onClick={() => handleDeleteActivity(activity.id)}>Delete</button>
           </li>
         ))}
